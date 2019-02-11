573328ca97ec9b92e6f3d098d657a3dbe3008bf4
Stephen Gibson <gibson.stephen92@gmail.com>
2019-02-11 13:43:43 +0000

    Now conforms to PEDMAS and brackets priority is implemented and working

--- calc.rb
+++ calc.rb
@@ -12,7 +12,7 @@ $options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: calc.rb [options]"
   $options[:operation] = "No formula specified"
-  opts.on("-o", "--operation=o", "Takes a mathematical formula and returns an answer, it can add, subtract, divide and multiply.") do |o|
+  opts.on("-o", "--operation=o", "Takes a mathematical formula and returns an answer, it can add, subtract, divide and multiply and conforms to PEDMA. Every non number character must be separated by a space") do |o|
     $options[:operation] = o
   end
   $options[:verbose] = false
@@ -23,6 +23,7 @@ OptionParser.new do |opts|
   end
   opts.on("-h", "--help", "Display this screen") do
     puts opts
+    exit
   end
 end.parse!
 
@@ -55,6 +56,16 @@ def validate_number(string)
   return string.to_f.to_s == string
 end
 
+def contains_operator(s, *operators)
+  (0..s.length - 1).each do |i|
+    if operators.include?(s[i])
+      $logger.debug("Operator #{s[i]} is in the valid list if operators (#{operators})")
+      return true
+    end
+  end
+  return false
+end
+
 # Converts all the integers in the array to floats, this makes it easier to code, not really for anything else
 def convert_ints_to_floats(s)
   $logger.debug("Converting all integers in #{s} to floats")
@@ -69,12 +80,20 @@ def convert_ints_to_floats(s)
 end
 
 def assign_operands(arr, i)
+  left = validate_number(arr[i - 1]) ? arr[i - 1] : false
+  right = validate_number(arr[i + 1]) ? arr[i + 1] : false
+  if !left or !right
+    $logger.info("Attempt to assign #{left ? "left" : "right"} operand failed, '#{!left ? arr[i - 1] : arr[i + 1]}' is not a number. Check your spacing")
+    exit
+  end
   $logger.debug("Assigning #{arr[i - 1]} to left operand and #{arr[ i + 1]} to right operand")
   return arr[i - 1], arr[i + 1]
 end
 
 # Calculates invidual blocks of operator, such as 5 * 3 or 5 + 5
 def calculate_blocks(s, *operators)
+  # Always delete all removable elements before attempting to carry out calculations
+  s.delete("R")
   op_index = ""
   $logger.debug("Calculating formula blocks, the data is #{s}")
   s.each_with_index { |v, i|
@@ -84,7 +103,7 @@ def calculate_blocks(s, *operators)
       op_index = i
       # Important check, makes sure that there are no double operators or missing numbers
       if i == 0 or (i + 1) > (s.length - 1)
-        $logger.debug("No #{i == 0 ? "left" : "right"} operand for operator '#{v}', found at index '#{i}'")
+        $logger.info("No #{i == 0 ? "left" : "right"} operand for operator '#{v}', found at index '#{i}'")
         exit
       end
       left_operand, right_operand = assign_operands(s, i)
@@ -92,25 +111,61 @@ def calculate_blocks(s, *operators)
       s[op_index] = $operations[s[op_index]].call(left_operand.to_f, right_operand.to_f).to_s
       s[i - 1] = "R"
       s[i + 1] = "R"
-      $logger.debug("Removing elements at index #{i - 1} and #{i + 1}")
       s.delete("R")
+      $logger.debug("Removing elements at index #{i - 1} and #{i + 1}")
       break
     end
   }
+  # Recursion baby. Continue to run this until all valid operators have been
+  # performed on
+  if(contains_operator(s, *operators))
+    calculate_blocks(s, *operators)
+  end
 end
+
+# Find all the brackets (open and closed) to conform to PEDMAS
+def find_brackets(s, i)
+  nested_s = []
+  n_nests = 0
+  left_bracket_i = 0
+  right_bracket_i = 0
+  $logger.debug("Finding brackets in formula '#{s}'' starting at index '#{i}', this is recursive so don't panic if it calls multiple times")
+  # Find brackets in s
+  (i..s.length - 1).each.with_index(i) { |y|
+    if s[y] == "("
+      $logger.debug("Found open bracket at index '#{y}', flagging it for removal")
+      s[y] = "R"
+      # We call this recursively to get into the deepest nest of brackets
+      find_brackets(s, y + 1)
+    end
+    if s[y] == ")"
+      $logger.debug("Found close bracket at index '#{y}', flagging it for removal")
+      s[y] = "R"
+      break
+    end
+    nested_s.push("#{s[y]}")
+  }
+  #find_brackets(nested_s, left_bracket_i + 1)
+  $logger.debug("Found this formula nested in brackets #{nested_s}")
+  # do all div/mul
+  calculate_blocks(nested_s, "*", "/")
+  # do all add/sub
+  calculate_blocks(nested_s, "+", "-")
+end
+
 
 # Important, everything needs to be floats
 s = convert_ints_to_floats(s)
 
+# Do all brackets first
+find_brackets(s, 0)
+
 # do all div/mul
-while s.include?("*") or s.include?("/") do
-  calculate_blocks(s, "*", "/")
-end
+calculate_blocks(s, "*", "/")
 
 # do all add/sub
-while s.include?("+") or s.include?("-") do
-  calculate_blocks(s, "+", "-")
-end
+calculate_blocks(s, "+", "-")
+
 
 # Output result
 puts "Result: #{s[0]}"
