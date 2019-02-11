65a98e0b64634e24349337597628e13340799851
Stephen Gibson <gibson.stephen92@gmail.com>
2019-02-11 09:49:07 +0000

    Removed redundant loops from calculate_blocks function. Removed redundant log_verbose function and now using  Ruby Logger class. Calculator works with div/mul/add/sub and it respects PEDMAS except for brackets

--- calc.rb
+++ calc.rb
@@ -1,6 +1,11 @@
+#!/bin/ruby
 # More advanced version of the basic calculator, allows you to run it with args and pass in a single mathematical operation
-# This works by passing in a mathem formula which is then interpreted and calculated
 require 'optparse'
+require 'logger'
+
+# Default logger
+$logger = Logger.new(STDOUT, datetime_format: '%H:%M:%S:%L')
+$logger.level = Logger::INFO
 
 operation = nil
 $options = {}
@@ -15,6 +20,8 @@ OptionParser.new do |opts|
   $options[:verbose] = false
   opts.on("-v", "--verbose", "Output full log info") do
     $options[:verbose] = true
+    # Set log level to debug for verbose
+    $logger.level = Logger::DEBUG
   end
 
   opts.on("-h", "--help", "Display this screen") do
@@ -22,33 +29,24 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-# Checks if the calculator is verbose and prints whater is passed in
-def log_verbose(output)
-  if $options[:verbose] == true
-    t = Time.now.strftime("%H:%M:%S:%L -")
-    # t = t.strftime("%H:%M:%S:%L")
-    puts "#{t} #{output}"
-  end
-end
-
-puts "Being verbose!" if $options[:verbose]
-
 if $options[:operation] == "No formula specified"
-  log_verbose("No option has been specified and no calulation can take place, exiting")
+  $logger.info "No option has been specified and no calulation can take place, exiting"
   exit
 end
 
-log_verbose("Starting calculation")
-log_verbose("Found formula '#{$options[:operation]}', carrying out calculation")
+$logger.debug("Starting in verbose mode")
 
-log_verbose("Populating lookup tables")
-add = -> (n1, n2) { log_verbose("Adding #{n1} and #{n2}"); return n1 + n2}
-sub = -> (n1, n2) { log_verbose("Subtracing #{n1} and #{n2}"); return n1 - n2}
-mul = -> (n1, n2) { log_verbose("Multiplying #{n1} and #{n2}"); return n1 * n2}
-div = -> (n1, n2) { log_verbose("Dividing #{n1} and #{n2}"); return n1 / n2}
+$logger.debug("Starting calculation")
+$logger.debug("Found formula '#{$options[:operation]}', carrying out calculation")
+
+$logger.debug("Populating lookup tables")
+add = -> (n1, n2) { $logger.debug("Adding #{n1} and #{n2}"); return n1 + n2}
+sub = -> (n1, n2) { $logger.debug("Subtracing #{n1} and #{n2}"); return n1 - n2}
+mul = -> (n1, n2) { $logger.debug("Multiplying #{n1} and #{n2}"); return n1 * n2}
+div = -> (n1, n2) { $logger.debug("Dividing #{n1} and #{n2}"); return n1 / n2}
 # Lookup table
 $operations = {"+" => add, "-" => sub, "*" => mul, "/" => div}
-log_verbose("Lookup tables populated")
+$logger.debug("Lookup tables populated")
 # Need to remember these for string scanning
 #$first_n = "[NOT SET]", $second_n = "[NOT SET]", $block_n = "", $op = "[NOT SET]"
 # Assign to shorthand
@@ -56,75 +54,68 @@ s = $options[:operation].split(" ")
 
 # Checks if a given string contains a valid number. Only floats are valid
 def validate_number(string)
-  log_verbose("Checking if string '#{string}' is a valid number: #{string.to_f.to_s == string}")
+  $logger.debug("Checking if string '#{string}' is a valid number: #{string.to_f.to_s == string}")
   return string.to_f.to_s == string
 end
 
 # Converts all the integers in the array to floats, this makes it easier to code, not really for anything else
 def convert_ints_to_floats(s)
-  log_verbose("Converting all integers in #{s} to floats")
+  $logger.debug("Converting all integers in #{s} to floats")
   (0..s.length - 1).each do |i|
     c = s[i]
       if c.to_i.to_s == c
         s[i] = c.to_f.to_s
       end
   end
-  log_verbose("Every integer in #{s} is now a float")
+  $logger.debug("Every integer in #{s} is now a float")
   return s
 end
 
-# Calculates all the multiplication and divisions first
-def calculate_mul_div_blocks(s)
-  first_n = ""
-  second_n = ""
+def assign_operands(arr, i)
+  $logger.debug("Assigning #{arr[i - 1]} to left operand and #{arr[ i + 1]} to right operand")
+  return arr[i - 1], arr[i + 1]
+end
+
+# Calculates invidual blocks of operator, such as 5 * 3 or 5 + 5
+def calculate_blocks(s, *operators)
   op_index = ""
-  log_verbose("Calculating formula blocks, the data is #{s}\n\n")
+  $logger.debug("Calculating formula blocks, the data is #{s}")
   s.each_with_index { |v, i|
-    log_verbose("Checking index #{i} in array, value present #{v}")
-    if v == "*" or v == "/"
-      log_verbose("Found operator '#{v}'")
+    $logger.debug("Checking index #{i} in array, value present #{v}")
+    if operators.include?(v)
+      $logger.debug("Found operator '#{v}'")
       op_index = i
-      # Find left operand
-      range = i-1..0
-      (range.first).downto(range.last).each { |ln|
-        log_verbose("Scanning left from index #{ln}, current character #{s[ln]}")
-        if validate_number(s[ln])
-          first_n = s[ln]
-          s[ln] = "REMOVE"
-        else
-          log_verbose("Left operand: #{first_n}")
-          break
-        end
-      }
-      # First right operand
-      (i + 1..s.length - 1).each do |rn|
-        log_verbose("Scanning right from index #{rn}, current character #{s[rn]}")
-        if validate_number(s[rn])
-          second_n = s[rn]
-          s[rn] = "REMOVE"
-        else
-          log_verbose("Right operand: #{second_n}")
-          # Calculate using the left and right operand with the found operator and replace it in the array
-          result = $operations[s[op_index]].call(first_n.to_f, second_n.to_f).to_s
-          s[op_index] = result
-          log_verbose("Removing empty elements")
-          s.delete("REMOVE")
-          break
-        end
+      # Important check, makes sure that there are no double operators or missing numbers
+      if i == 0 or (i + 1) > (s.length - 1)
+        $logger.debug("No #{i == 0 ? "left" : "right"} operand for operator '#{v}', found at index '#{i}'")
+        exit
       end
+      left_operand, right_operand = assign_operands(s, i)
+      # Replaces the operator found with the answer found using the left and right operator
+      s[op_index] = $operations[s[op_index]].call(left_operand.to_f, right_operand.to_f).to_s
+      s[i - 1] = "R"
+      s[i + 1] = "R"
+      $logger.debug("Removing elements at index #{i - 1} and #{i + 1}")
+      s.delete("R")
+      break
     end
   }
-  return s
 end
 
 # Important, everything needs to be floats
 s = convert_ints_to_floats(s)
 
+# do all div/mul
 while s.include?("*") or s.include?("/") do
-  s = calculate_mul_div_blocks(s)
+  calculate_blocks(s, "*", "/")
 end
 
-puts "The calculation is now complete, the result was\n'#{s}'"
+# do all add/sub
+while s.include?("+") or s.include?("-") do
+  calculate_blocks(s, "+", "-")
+end
+
+ puts "Result: #{s[0]}"
 
 # if first_n != "[NOT SET]"
 #   be_verbose(options, "Found end of second number")
